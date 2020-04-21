23f4079b8fc4e69a7a66d1efdf035480fd943d71
Trevor Landau <landau@users.noreply.github.com>
2020-04-21 07:07:25 -0400

    chore: remove bulky deps

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const predicate = require('predicate');
-const { describe, it } = require('mocha');
-const dropWhile = require('.');
+const assert = require("assert");
+const dropWhile = require("./");
 
-describe('dropWhile', () => {
-  it('should drop till negative numbers stop', () => {
-    const arr = [-1, -2, -6, -7, 1, 2, 3, 4, -5, -6, 0, 1];
-    const expect = [1, 2, 3, 4, -5, -6, 0, 1];
-    assert.deepEqual(dropWhile(arr, predicate.neg), expect);
-  });
-});
+const isNegative = (n) => n < 0;
+
+const arr = [-1, -2, -6, -7, 1, 2, 3, 4, -5, -6, 0, 1];
+const expect = [1, 2, 3, 4, -5, -6, 0, 1];
+assert.deepEqual(
+  dropWhile(arr, isNegative),
+  expect,
+  "should drop till negative numbers stop"
+);
